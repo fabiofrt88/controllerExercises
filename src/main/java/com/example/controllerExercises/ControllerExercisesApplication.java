46946d5987d9ff0f615f9108f154b79943180c8f
Fabio Frattarelli <fabiofrt@outlook.it>
2023-02-24 12:35:55 +0100

    first commit

---
+++ src/main/java/com/example/controllerExercises/ControllerExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.example.controllerExercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControllerExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControllerExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/controllerExercises/ControllerExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.controllerExercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
